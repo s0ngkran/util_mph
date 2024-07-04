b06b5990aa3a9e7c2012adad26e158f335e3bcf2
s0ngkran <jinnawat8@gmail.com>
2024-07-04 13:35:20 +0700

    read data plain black

--- read_data.py
+++ read_data.py
@@ -1,17 +1,19 @@
 import json
 from utils.mph_input import MPHInput
 from utils.read_config import read_config
+from collections import Counter
 
 def read_data():
-    path = '../data_zip/poh_vr_1k/keypoints.json'
+    path = '../data_zip/poh_plain_vs_natural/replaced/replaced_bg.poh_black_testing.json'
     with open(path, 'r') as f:
         data = json.load(f)
     folder = read_config()
 
+
     mph_input_list = []
-    for k,v in data.items():
+    for v in data:
         img_name = v['img_path'].split('/')[-1]
-        gt = int(v['gt']) 
+        gt = ord(v['gt']) - 65
         gt = custom_gt_for_vr_poh_1k(gt)
         obj = MPHInput(folder, img_name,  gt)
         mph_input_list.append(obj)
@@ -36,18 +38,19 @@ def custom_gt_for_vr_poh_1k(gt):
 
     FROM 
 
-    3 6 7 [ ]
+    3 5 7 [ ]
     | | | |
     | | | |
     | | | |
-    4 5 8 9
+    4 6 8 9
 2
 
   1    10
 
        0
     '''
-    convert_key = [0,1,2,5,4,6,7,9,8,10,3]
+    #              0 1 2 3 4 5 6 7 8 9 10
+    convert_key = [0,1,2,5,4,7,6,9,8,10,3]
     return convert_key[gt]
 
 def test():

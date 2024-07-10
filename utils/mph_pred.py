0affe4a1587317b5ee7f3960e5e484a7a3d19449
s0ngkran <jinnawat8@gmail.com>
2024-07-10 14:24:17 +0700

    try

--- utils/mph_pred.py
+++ utils/mph_pred.py
@@ -12,6 +12,7 @@ def extract_hand_keypoints(img_path):
     
     # Read the image
     image = cv2.imread(img_path)
+    h, w, _ = image.shape
     if image is None:
         raise ValueError("Image not found or unable to read the image file.")
     
@@ -22,18 +23,21 @@ def extract_hand_keypoints(img_path):
     results = hands.process(image_rgb)
     
     keypoints = []
+    hds = []
     if results.multi_hand_landmarks:
-        for hand_landmarks in results.multi_hand_landmarks:
-            for landmark in hand_landmarks.landmark:
+        for hand_landmarks, world, handedness in zip(results.multi_hand_landmarks, results.multi_hand_world_landmarks, results.multi_handedness):
+            for handed in handedness.classification:
+                hds.append((handed.index, handed.label))
+            for landmark, world_ in zip(hand_landmarks.landmark, world.landmark):
                 # Convert normalized coordinates to pixel values
-                h, w, _ = image.shape
                 x = float(landmark.x * w)
                 y = float(landmark.y * h)
-                keypoints.append((x, y))
-    
+                # z = float(landmark.z)
+                z = float(world_.z)
+                keypoints.append((x, y, z))
     # Release the MediaPipe resources
     hands.close()
-    return keypoints, image
+    return keypoints, image, hds
 
 def get_file_names(folder_path=""):
     names = os.listdir(folder_path)
@@ -54,11 +58,17 @@ def mph_pred_folder():
     for i, name in enumerate(names):
         print(f'{i}/{n}')
         path = os.path.join(folder, name)
-        keypoints, img = extract_hand_keypoints(path)
+        keypoints, img, hds = extract_hand_keypoints(path)
         key = name
-        value = keypoints
+        # value = keypoints
+        hds = sorted(hds, key=lambda x: x[0], reverse=False)
+        handedness = [d[1][0] for d in hds]
+        value = {
+            'keypoints': keypoints,
+            'handedness': handedness,
+        }
         out[key] = value
-    save_json(out, 'mph_keypoints.json')
+    save_json()
 
 def test():
     mph_pred_folder()
